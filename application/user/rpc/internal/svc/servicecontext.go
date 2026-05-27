8ef9ade9f177f4d20eb8b7f4a5c7124800d32b99
POTATO <pot88017@gmail.com>
2026-05-27 18:02:50 +0800

    feat: integrate MySQL and Redis cache support into user RPC service context

--- application/user/rpc/internal/svc/servicecontext.go
+++ application/user/rpc/internal/svc/servicecontext.go
@@ -3,15 +3,20 @@ package svc
 import (
 	"zhihu/application/user/rpc/internal/config"
 	"zhihu/application/user/rpc/internal/model"
+
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 type ServiceContext struct {
 	UserModel model.UserModel
-	Config config.Config
+	Config    config.Config
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	conn := sqlx.NewMysql(c.DataSource)
+
 	return &ServiceContext{
-		Config: c,
+		Config:    c,
+		UserModel: model.NewUserModel(conn, c.CacheRedis),
 	}
 }
